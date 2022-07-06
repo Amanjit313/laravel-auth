776411f6bad804d21f52d74360647b9dce338c04
Amanjit313 <amanjitsingh313@gmail.com>
2022-07-06 16:08:22 +0200

    aggiunti nuovi elementi al mio db e stampati correttamente (la show)

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
 
-<h1>Post details</h1>
+<h1>Author Post Details</h1>
 
-<h3>Name Author: {{ $post->name }}</h3>
-<h3>Location Author: {{ $post->location }}</h3>
-<h3>Post published at: {{ $post->time_post }}</h3>
+<h3>Name: {{ $post->name }}</h3>
+<h3>Location: {{ $post->location }}</h3>
+<h3>Email: {{ $post->email }}</h3>
 <h3>ID Post: {{ $post->id }}</h3>
 
 <a class="btn btn-primary" href="{{ route('admin.posts.index') }}">BACK</a>
